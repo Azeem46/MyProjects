03841de5087a150aff6e268d57906cb7c3344721
Azeem46 <shaikhazeem4646@gmail.com>
2023-09-23 18:30:51 +0530

    responsiveness

--- src/components/Navbar/RightContent/icons.tsx
+++ src/components/Navbar/RightContent/icons.tsx
@@ -11,23 +11,21 @@ import {
 const Icons:React.FC = () => {
     
     return (
-        <Flex>
-            <Flex 
-            align="center"
-             borderRight="1px solid" 
-             borderColor="gray.200"
-             >
-                <Flex 
-                display={{ base: "none", md: "flex"}} 
-                mr={1.5}
-                 ml={1.5} 
-                 padding={1}
-                  cursor="pointer"
-                  borderRadius={4}
-                  _hover={{
-                    bg: "gray.200"
-                  }}
-                  >
+       <Flex alignItems="center" flexGrow={1}>
+      <Flex
+        display={{ base: "none", md: "flex" }}
+        alignItems="center"
+        borderRight="1px solid"
+        borderColor="gray.200"
+      >
+        <Flex
+          mr={1.5}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: "gray.200" }}
+        >
                     <Icon as={BsArrowUpRightCircle} fontSize={20} />
                 </Flex>
                 <Flex 
