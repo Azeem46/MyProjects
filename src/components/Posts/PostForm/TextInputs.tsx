3fc7fc3c5c95fed30cc89bc07ac7664e46fefd06
Azeem46 <shaikhazeem4646@gmail.com>
2024-05-02 22:12:34 +0530

    changes2

--- src/components/Posts/PostForm/TextInputs.tsx
+++ src/components/Posts/PostForm/TextInputs.tsx
@@ -1,27 +1,7 @@
 import { Button, Flex, Input, Stack } from '@chakra-ui/react';
-import React, { useState } from 'react';
-import ReactQuill from 'react-quill';
+import React, { useState, useEffect } from 'react';
+import dynamic from 'next/dynamic'; // Import dynamic from Next.js
 import 'react-quill/dist/quill.snow.css'; // Import Quill styles
-
-// Import Quill
-import Quill from 'quill';
-
-// Set default Quill modules
-const defaultModules = {
-  toolbar: [
-    [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
-    [{size: []}],
-    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-    [{'list': 'ordered'}, {'list': 'bullet'}, 
-     {'indent': '-1'}, {'indent': '+1'}],
-    ['link', 'image', 'video'],
-    ['clean']
-  ],
-  clipboard: {
-    // toggle to add extra line breaks when pasting HTML:
-    matchVisual: false,
-  }
-};
 
 type TextInputsProps = {
     textInputs: {
@@ -42,6 +22,31 @@ const TextInputs: React.FC<TextInputsProps> = ({
     loading,
 }) => {
     const [body, setBody] = useState(textInputs.body);
+    const defaultModules = {
+  toolbar: [
+    [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+    [{size: []}],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [{'list': 'ordered'}, {'list': 'bullet'}, 
+     {'indent': '-1'}, {'indent': '+1'}],
+    ['link', 'image', 'video'],
+    ['clean']
+  ],
+  clipboard: {
+    // toggle to add extra line breaks when pasting HTML:
+    matchVisual: false,
+  }
+};
+
+    // Ensure Quill.js is loaded on the client-side
+    const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            // Import Quill here if needed
+            // const Quill = require('quill');
+        }
+    }, []);
 
     const handleBodyChange = (value: string) => {
         setBody(value);
@@ -64,32 +69,35 @@ const TextInputs: React.FC<TextInputsProps> = ({
                     borderColor: 'black',
                 }}
             />
-            <div style={{ overflow: 'auto', maxHeight: 300 , maxWidth:'700px' }}>
-                 <ReactQuill
-                    value={body}
-                    onChange={handleBodyChange}
-                    modules={defaultModules} // Pass default modules here
-                    theme="snow"
-                    placeholder="Text (optional)"
-                    style={{
-                        fontSize: '10pt',
-                        height: '200px'
-                    }}
-                />
+            <div style={{ overflow: 'auto', maxHeight: 300, maxWidth: '700px' }}>
+                {/* Render ReactQuill component only on the client-side */}
+                {ReactQuill && (
+                    <ReactQuill
+                        value={body}
+                        onChange={handleBodyChange}
+                        modules={defaultModules} // Pass default modules here
+                        theme="snow"
+                        placeholder="Text (optional)"
+                        style={{
+                            fontSize: '10pt',
+                            height: '200px'
+                        }}
+                    />
+                )}
             </div>
-               <div>
+            <div>
                 <Flex justify="flex-end">
-                <Button
-                    height="34px"
-                    padding="0px 30px"
-                    disabled={!textInputs.title}
-                    isLoading={loading}
-                    onClick={handleCreatePost}
-                >
-                    Post
-                </Button>
-            </Flex>
-               </div>
+                    <Button
+                        height="34px"
+                        padding="0px 30px"
+                        disabled={!textInputs.title}
+                        isLoading={loading}
+                        onClick={handleCreatePost}
+                    >
+                        Post
+                    </Button>
+                </Flex>
+            </div>
         </Stack>
     );
 };
